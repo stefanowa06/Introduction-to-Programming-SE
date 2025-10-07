308f65d8f394fb7c4f93a6adda01d148130f6924
stefanowa06 <stefanowa06@gmail.com>
2025-10-07 21:49:01 +0300

    задачи без 3 и 7

---
+++ hw-10-7-25/hw-10-7-25/hw-10-7-25.cpp
@@ -0,0 +1,73 @@
+﻿// hw-10-7-25.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{/* zad 1 ot ostatuchnoto delenie
+    std::cout << "Vavedi chislo\n";
+    int x;
+    std::cin >> x;
+    int result = 1 - (x % 2);
+    std::cout << result;
+    return 0;
+ */ 
+ /* zad 2
+    std::cout << "Vavedi tricifreno chislo\n";
+    int x;
+    std::cin >> x;
+    int obratno;
+    int stotici = x / 100;//ostava purvoto chislo
+    int desetici = (x / 10) % 10;
+    int edinici = x % 10;
+    obratno = edinici * 100 + desetici * 10 + stotici;
+    std::cout << obratno + 1;
+    */
+    /* 3та не знам
+    int a, b;
+    std::cin >> a;
+    std::cin >> b;
+    bool c = a>b;
+    c == a;
+    bool d = b > a;
+    d == b;
+    std::cout << c;
+    std::cout << d;
+    */
+    /*4та задача
+    int ocenki;
+    std::cin >> ocenki;
+    bool b = ocenki <= 6 && ocenki >= 2;
+    std::cout << b;
+    */
+    /*5та задача
+    int x;
+    std::cin >> x;
+    int y = x / 10;
+    std::cout << y;
+    */
+    /*
+    int a, b;
+    std::cin >> a;
+    std::cin >> b;
+    int skobi1 = a + b;
+    int skobi2 = a - b;
+    int result = skobi1 * skobi1 * skobi1 * skobi1 - skobi2 * skobi2;
+    std::cout << result;
+    */
+    /*
+    int leva;
+    std::cin >> leva;
+    */
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
